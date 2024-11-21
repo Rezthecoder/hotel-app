25d2a405813bc7fdc3d5187953bf67c4e07be1d3
rez <noobprogramming111@gmail.com>
2024-11-21 09:34:07 +0900

    added starter files

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+
+function Dashboard() {
+  return (
+    <div>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <p>TEST</p>
+      </Row>
+    </div>
+  );
+}
+
+export default Dashboard;
